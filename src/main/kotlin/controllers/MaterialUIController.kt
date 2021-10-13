4e2fc39a6f2ac1213d5f4a82677a6d93ea5390de
MrSpeedy68 <47033681+MrSpeedy68@users.noreply.github.com>
2021-10-13 11:01:29 +0100

    Adding of a User in a UI

---
+++ src/main/kotlin/controllers/MaterialUIController.kt
@@ -0,0 +1,13 @@
+package controllers
+
+import tornadofx.*
+
+class MaterialUIController : Controller() {
+
+    fun add(_Name : String, _labour : Double, _energy : Double, _currency : String){
+
+//        var aPlacemark = PlacemarkModel(title = _title, description = _description)
+//        placemarks.create(aPlacemark)
+//        logger.info("Placemark Added")
+    }
+}
--- src/main/kotlin/screens/UserScreen.kt
+++ src/main/kotlin/screens/UserScreen.kt
@@ -1,23 +1,53 @@
 package screens
 
 import controllers.MenuUIController
+import controllers.UserUIController
+import javafx.beans.property.SimpleDoubleProperty
+import javafx.beans.property.SimpleStringProperty
 import javafx.geometry.Orientation
+import jdk.nashorn.internal.runtime.JSType.toDouble
 import tornadofx.*
 
 class UserScreen : View("User Settings") {
 
-    val menuUIController : MenuUIController by inject()
-
+    val userUIController : UserUIController by inject()
+    val model = ViewModel()
+    val _name = model.bind { SimpleStringProperty() }
+    val _currency = model.bind { SimpleStringProperty() }
+    val _labour = model.bind { SimpleDoubleProperty() }
+    val _energy = model.bind { SimpleDoubleProperty() }
 
     override val root = form {
-        setPrefSize(400.0, 400.0)
+        setPrefSize(600.0, 400.0)
         fieldset(labelPosition = Orientation.VERTICAL) {
+            field("Name") {
+                textfield(_name).required()
+            }
+            field("Labour Cost") {
+                textfield(_labour).required()
+            }
+            field("Energy Cost") {
+                textfield(_energy).required()
+            }
+            field("Currency") {
+                textfield(_currency).required()
+            }
+            button("Add") {
+                enableWhen(model.valid)
+                isDefaultButton = true
+                useMaxWidth = true
+                action {
+                    runAsyncWithProgress {
+                        userUIController.add(_name.value.toString(),_labour.value.toDouble(),_energy.value.toDouble(),_currency.value.toString())
+                    }
+                }
+            }
             text("")
             button("Close") {
                 useMaxWidth = true
                 action {
                     runAsyncWithProgress {
-                        menuUIController.closeUser()
+                        userUIController.closeUser()
                     }
                 }
             }
